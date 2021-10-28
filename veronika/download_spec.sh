f5143d77d1824e337b0d3feee077ccf03fd0f06a
Rinat Tagirov <tagirovrinat@gmail.com>
2021-10-29 01:30:16 +0300

    minor changes

---
+++ veronika/download_spec.sh
@@ -0,0 +1,4 @@
+scp $hpc:/rds/general/user/rtagirov/ephemeral/runs/nessy/G2V/ssd/222809/1/lte/2/spec/G2V.ssd.222809.1.lte.2.npz ./
+scp $hpc:/rds/general/user/rtagirov/ephemeral/runs/nessy/G2V/ssd/222809/1/lte/10/spec/G2V.ssd.222809.1.lte.10.npz ./
+scp $hpc:/rds/general/user/rtagirov/ephemeral/runs/nessy/G2V/300G/627321/1/lte/2/spec/G2V.300G.627321.1.lte.2.npz ./
+scp $hpc:/rds/general/user/rtagirov/ephemeral/runs/nessy/G2V/300G/627321/1/lte/10/spec/G2V.300G.627321.1.lte.10.npz ./
